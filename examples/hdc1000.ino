e4935538a9fa0cb5b5b25256af26366bb2dab3db
Yuichi Tateno <hotchpotch@gmail.com>
2015-10-07 23:14:01 +0900

    Update example

--- examples/hdc1000.ino
+++ examples/hdc1000.ino
@@ -5,14 +5,27 @@ HDC1000 hdc1000;
 
 void setup() {
   Serial.begin(9600);
+  // hdc1000.setRdyPin(PIN_NO); // use Rdy Pin
   hdc1000.begin();
 }
 
 void loop() {
   float temp = hdc1000.getTemperature();
+
+  if (temp == HDC1000_ERROR_CODE) {
+    hdc1000.begin(); // retry once
+    temp = hdc1000.getTemperature();
+  }
   Serial.print("Temperature: ");
   Serial.println(temp);
+
   float humi = hdc1000.getHumidity();
+
+  if (humi== HDC1000_ERROR_CODE) {
+    hdc1000.begin(); // retry once
+    humi = hdc1000.getTemperature();
+  }
+
   Serial.print("Humidity: ");
   Serial.println(humi);
   delay(1000);
